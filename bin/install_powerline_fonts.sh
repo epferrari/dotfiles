1fd3afe7d4d6cf637993aacbb7da9fef991011b0
Kurt Preston <kpreston@opinionlab.com>
2014-10-15 15:26:36 -0500

    Simplifying powerline font installation since install script was merged into project

--- bin/install_powerline_fonts.sh
+++ bin/install_powerline_fonts.sh
@@ -3,26 +3,10 @@
 # Configure Directories
 powerline_dir="/tmp/powerline-fonts"
 
-if [[ `uname` == 'Darwin' ]]; then
-  # MacOS
-  font_dir="$HOME/Library/Fonts"
-else
-  # Linux
-  font_dir="$HOME/.fonts"
-  mkdir -p $font_dir
-fi
-
 # Download fonts
 git clone https://github.com/Lokaltog/powerline-fonts.git $powerline_dir
 
-# Copy all fonts to user fonts directory
-find_command="find $powerline_dir -name '*.[o,t]tf' -type f -print0"
-eval $find_command | xargs -0 -I % cp % $font_dir/
-
-# Reset font cache on Linux
-if [[ -n `which fc-cache` ]]; then
-  fc-cache -f ~/.fonts/
-fi
+# Install fonts
+$powerline_dir/install.sh
 
 rm -rf $powerline_dir
-echo "All Powerline fonts installed to $font_dir"
