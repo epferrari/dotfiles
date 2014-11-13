6e40f84c576bdcefaf87309343d8ce4b6d3858bc
Kurt Preston <kpreston@opinionlab.com>
2014-11-12 16:47:19 -0800

    Store powerline repo to prevent re-download

--- bin/install_powerline_fonts.sh
+++ bin/install_powerline_fonts.sh
@@ -2,13 +2,14 @@
 
 # Only install fonts if in a Linux Windows Manager or MacOS
 if [[ (-z "$GDMSESSION") || (`uname` == "Darwin") ]]; then
-  powerline_dir="/tmp/powerline-fonts"
+
+  powerline_install_dir="$( cd "$( dirname "$0" )" && pwd )"
+  powerline_dir="$powerline_install_dir/repos"
 
   # Download fonts
   git clone https://github.com/Lokaltog/powerline-fonts.git $powerline_dir
+  cd $powerline_dir && git pull origin master
 
   # Install fonts
   $powerline_dir/install.sh
-
-  rm -rf $powerline_dir
 fi
