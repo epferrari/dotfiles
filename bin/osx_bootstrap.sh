dd2d39b8fcdaac5d3f660b9fb473a03a79fbfca5
Kurt Preston <kpreston@opinionlab.com>
2015-09-16 13:18:29 -0500

    Creating basic OS X bootstrap script

---
+++ bin/osx_bootstrap.sh
@@ -0,0 +1,15 @@
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew install git
+brew install wget
+
+# Install brew-cask
+brew install caskroom/cask/brew-cask
+
+# Install apps
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install keepass-x
+brew cask install iterm2
+brew cask install mattr-slate
+brew cask install macvim
