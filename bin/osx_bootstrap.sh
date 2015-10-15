1469f5d3e378b2df99dec9f62a2100d7a8df8729
Kurt Preston <github@kurtpreston.com>
2015-10-14 23:00:26 -0500

    Updating OSX bootstrap script

--- bin/osx_bootstrap.sh
+++ bin/osx_bootstrap.sh
@@ -1,15 +1,26 @@
+# Get the dotfiles
+mkdir ~/Code
+cd ~/Code
+git clone https://github.com/KurtPreston/dotfiles.git
+cd dotfiles
+rake install
+
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew install git
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install command line tools
 brew install wget
 
 # Install brew-cask
 brew install caskroom/cask/brew-cask
 
 # Install apps
+brew cask install mattr-slate
 brew cask install dropbox
 brew cask install google-chrome
-brew cask install keepass-x
+brew cask install keepassx
 brew cask install iterm2
-brew cask install mattr-slate
 brew cask install macvim
+
+# Get RVM
+\curl -sSL https://get.rvm.io | bash -s stable
