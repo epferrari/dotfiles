bafa2cb275b81665e142746f8b0ce4e7dfe11392
Kurt Preston <kpreston@drw.com>
2016-11-03 16:57:24 -0500

    Updating OSX bootstrapping

--- bin/osx_bootstrap.sh
+++ bin/osx_bootstrap.sh
@@ -7,6 +7,19 @@ brew install nvm python
 brew install neovim/neovim/neovim
 pip install --upgrade neovim
 
+# RVM
+\curl -sSL https://get.rvm.io | bash -s stable
+rvm install ruby --latest
+gem install neovim
+
+# NVM
+brew install nvm
+mkdir -p ~/.nvm
+export NVM_DIR="/home/kurt/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
+nvm install node
+
 # Get the dotfiles
 mkdir ~/Code
 cd ~/Code
@@ -21,7 +34,3 @@ brew cask install google-chrome
 brew cask install dwihn0r-keepassx
 brew cask install iterm2
 brew cask install macvim
-
-# Get RVM
-\curl -sSL https://get.rvm.io | bash -s stable
-gem install neovim
