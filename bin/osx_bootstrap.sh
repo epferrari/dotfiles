c39fe411b5f1387272f4b10d2e046e7486177e09
Kurt Preston <kpreston@drw.com>
2016-11-03 16:42:45 -0500

    Updating OSX script for neovim and YCM

--- bin/osx_bootstrap.sh
+++ bin/osx_bootstrap.sh
@@ -1,18 +1,18 @@
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install command line tools
+brew install cmake wget
+brew install nvm python
+brew install neovim/neovim/neovim
+pip install --upgrade neovim
+
 # Get the dotfiles
 mkdir ~/Code
 cd ~/Code
 git clone https://github.com/KurtPreston/dotfiles.git
 cd dotfiles
 rake install
-
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install command line tools
-brew install wget
-
-# Install brew-cask
-brew install caskroom/cask/brew-cask
 
 # Install apps
 brew cask install mattr-slate
@@ -24,3 +24,4 @@ brew cask install macvim
 
 # Get RVM
 \curl -sSL https://get.rvm.io | bash -s stable
+gem install neovim
